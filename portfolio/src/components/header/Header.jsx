c51a5d89f5c6d5c76dc2f4193c9cda9c2e39ba75
Eric Furukawa <eric.furukawa@gmail.com>
2022-07-04 13:29:52 -0700

    added pic and styling to header

--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './header.css'
-
+import CTA from './CTA'
+import ME from '../../assets/portcut.png';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Eric K Furukawa</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="Eric K Furukawa" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
