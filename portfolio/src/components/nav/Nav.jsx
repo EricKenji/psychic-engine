91bf829ce74dc43e0cad2f7a2f3f4d67daa9a5ab
Eric Furukawa <eric.furukawa@gmail.com>
2022-07-05 13:11:16 -0700

    added navbar and navbar styling

--- portfolio/src/components/nav/Nav.jsx
+++ portfolio/src/components/nav/Nav.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './nav.css';
+import {FaHome} from 'react-icons/fa';
+import {BsPersonSquare, BsPaperclip} from 'react-icons/bs';
+import {AiFillContacts} from 'react-icons/ai';
 
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><FaHome/></a>
+      <a href="#about"><BsPersonSquare/></a>
+      <a href="#portfolio"><BsPaperclip/></a>
+      <a href="#contact"><AiFillContacts/></a>
+    </nav>
   )
 }
 
