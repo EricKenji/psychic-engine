8bae5b4fb60c026de037cfa6c4b4e57d4d831da0
Eric Furukawa <eric.furukawa@gmail.com>
2022-07-14 12:58:31 -0700

    finished portfolio section

--- portfolio/src/components/portfolio/Portfolio.jsx
+++ portfolio/src/components/portfolio/Portfolio.jsx
@@ -20,63 +20,68 @@ const Portfolio = () => {
             <img src={IMG1} alt="Whiskey Business" />
           </div>
           <h3>Cocktail Search Engine</h3>
-          <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">Github</a>
-          <a href="https://google.com" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/EricKenji/whiskey-business" className='btn' target='_blank' rel="noreferrer">Github</a>
+            <a href="https://arcane-shelf-84573.herokuapp.com/" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          </div>
         </article>
-      </div>
 
-      <div className="container portfolio__container">
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
-            <img src={IMG1} alt="Whiskey Business" />
+            <img src={IMG2} alt="Whiskey Business" />
           </div>
           <h3>Job Posting App</h3>
-          <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">Github</a>
-          <a href="https://google.com" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/EricKenji/collaborative-cargo" className='btn' target='_blank' rel="noreferrer">Github</a>
+            <a href="https://collaborative-cargo.herokuapp.com/" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          </div>
         </article>
-      </div>
+  
 
-      <div className="container portfolio__container">
+
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
-            <img src={IMG1} alt="Whiskey Business" />
+            <img src={IMG3} alt="Whiskey Business" />
           </div>
           <h3>Stock Tracker</h3>
-          <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">Github</a>
-          <a href="https://google.com" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/EricKenji/stock-tracker" className='btn' target='_blank' rel="noreferrer">Github</a>
+            <a href="https://brandonsauer8.github.io/stock-tracker/" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          </div>
         </article>
-      </div>
+  
 
-      <div className="container portfolio__container">
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
-            <img src={IMG1} alt="Whiskey Business" />
+            <img src={IMG4} alt="Whiskey Business" />
           </div>
           <h3>Budget Tracker</h3>
-          <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">Github</a>
-          <a href="https://google.com" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/EricKenji/expert-spork" className='btn' target='_blank' rel="noreferrer">Github</a>
+            <a href="https://immense-refuge-18672.herokuapp.com/" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          </div>
         </article>
-      </div>
 
-      <div className="container portfolio__container">
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
-            <img src={IMG1} alt="Whiskey Business" />
+            <img src={IMG5} alt="Whiskey Business" />
           </div>
           <h3>Blog Website</h3>
-          <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">Github</a>
-          <a href="https://google.com" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/EricKenji/tech-talk" className='btn' target='_blank' rel="noreferrer">Github</a>
+            <a href="https://shrouded-gorge-11182.herokuapp.com/post/4" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          </div>
         </article>
-      </div>
 
-      <div className="container portfolio__container">
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
-            <img src={IMG1} alt="Whiskey Business" />
+            <img src={IMG6} alt="Whiskey Business" />
           </div>
           <h3>Note Taker</h3>
-          <a href="https://github.com" className='btn' target='_blank' rel="noreferrer">Github</a>
-          <a href="https://google.com" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/EricKenji/note-keeper" className='btn' target='_blank' rel="noreferrer">Github</a>
+            <a href="https://nameless-retreat-41957.herokuapp.com/notes" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+          </div>
         </article>
       </div>
     </section>
